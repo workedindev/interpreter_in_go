64385ae7c643315adede25436e2350c7cebadf7a
workedindev <prox@workedindev.com>
2024-02-25 11:44:32 -0600

    completed until ch2.6: parsing expressions

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -8,9 +8,9 @@ import (
 
 func TestLetStatements(t *testing.T) {
 	input := `
-let x 5;
-let = 10;
-let 838383;
+let x = 5;
+let y = 10;
+let foobar = 838383;
 `
 
 	l := lexer.New(input)
@@ -84,6 +84,18 @@ return 993322;
 	if len(program.Statements) != 3 {
 		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
 			len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got=%q",
+				returnStmt.TokenLiteral())
+		}
 	}
 }
 
